ActiveBrainAtlas2/neuroglancer

commit ce282b4e231eb220bd5201534ac518741ed8f3b7

feat: add N5 datasource

src/neuroglancer/sliceview/backend_chunk_decoders/raw.ts
@@ -21,33 +21,34 @@ import {convertEndian16, convertEndian32, Endianness, ENDIANNESS} from 'neurogla
 import {prod3} from 'neuroglancer/util/geom';
 
 export function decodeRawChunk(
-    chunk: VolumeChunk, response: ArrayBuffer, endianness: Endianness = ENDIANNESS) {
+    chunk: VolumeChunk, response: ArrayBuffer, endianness: Endianness = ENDIANNESS,
+    byteOffset: number = 0, byteLength: number = response.byteLength) {
   let {spec} = chunk.source!;
   let {dataType} = spec;
   let numElements = prod3(chunk.chunkDataSize!);
   let bytesPerElement = DATA_TYPE_BYTES[dataType];
   let expectedBytes = numElements * bytesPerElement * spec.numChannels;
-  if (expectedBytes !== response.byteLength) {
+  if (expectedBytes !== byteLength) {
     throw new Error(
-        `Raw-format chunk is ${response.byteLength} bytes, ` +
+        `Raw-format chunk is ${byteLength} bytes, ` +
         `but ${numElements} * ${bytesPerElement} = ${expectedBytes} bytes are expected.`);
   }
   let data: ArrayBufferView;
   switch (dataType) {
     case DataType.UINT8:
-      data = new Uint8Array(response);
+      data = new Uint8Array(response, byteOffset, byteLength);
       break;
     case DataType.UINT16:
-      data = new Uint16Array(response);
+      data = new Uint16Array(response, byteOffset, byteLength / 2);
       convertEndian16(data, endianness);
       break;
     case DataType.UINT32:
     case DataType.UINT64:
-      data = new Uint32Array(response);
+      data = new Uint32Array(response, byteOffset, byteLength / 4);
       convertEndian32(data, endianness);
       break;
     case DataType.FLOAT32:
-      data = new Float32Array(response);
+      data = new Float32Array(response, byteOffset, byteLength / 2);
       convertEndian32(data, endianness);
       break;
     default:

config/webpack_helpers.js
@@ -45,6 +45,7 @@ const DEFAULT_DATA_SOURCES = exports.DEFAULT_DATA_SOURCES = [
   'neuroglancer/datasource/render',
   'neuroglancer/datasource/precomputed',
   'neuroglancer/datasource/nifti',
+  'neuroglancer/datasource/n5',
   {source: 'neuroglancer/datasource/vtk', register: null},
   {source: 'neuroglancer/datasource/csv', register: null},
 ];

src/neuroglancer/datasource/index.ts
@@ -111,7 +111,7 @@ export class DataSource extends RefCounted {
   description?: string;
 }
 
-const protocolPattern = /^(?:([a-zA-Z-+_]+):\/\/)?(.*)$/;
+const protocolPattern = /^(?:([a-zA-Z][a-zA-Z0-9-+_]*):\/\/)?(.*)$/;
 
 export class DataSourceProvider extends RefCounted {
   dataSources = new Map<string, Owned<DataSource>>();

src/neuroglancer/datasource/n5/backend.ts
@@ -1,0 +1,63 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {WithParameters} from 'neuroglancer/chunk_manager/backend';
+import {VolumeChunkEncoding, VolumeChunkSourceParameters} from 'neuroglancer/datasource/n5/base';
+import {decodeRawChunk} from 'neuroglancer/sliceview/backend_chunk_decoders/raw';
+import {VolumeChunk, VolumeChunkSource} from 'neuroglancer/sliceview/volume/backend';
+import {CancellationToken} from 'neuroglancer/util/cancellation';
+import {Endianness} from 'neuroglancer/util/endian';
+import {vec3} from 'neuroglancer/util/geom';
+import {openHttpRequest, sendHttpRequest} from 'neuroglancer/util/http_request';
+import {registerSharedObject} from 'neuroglancer/worker_rpc';
+import {inflate} from 'pako';
+
+function decodeChunk(chunk: VolumeChunk, response: ArrayBuffer, encoding: VolumeChunkEncoding) {
+  const dv = new DataView(response);
+  const mode = dv.getUint16(0, /*littleEndian=*/ false);
+  if (mode !== 0) {
+    throw new Error(`Unsupported mode: ${mode}.`);
+  }
+  const numDimensions = dv.getUint16(2, /*littleEndian=*/ false);
+  if (numDimensions !== 3) {
+    throw new Error(`Number of dimensions must be 3.`);
+  }
+  let offset = 4;
+  const shape = new Uint32Array(numDimensions);
+  for (let i = 0; i < numDimensions; ++i) {
+    shape[i] = dv.getUint32(offset, /*littleEndian=*/ false);
+    offset += 4;
+  }
+  chunk.chunkDataSize = vec3.fromValues(shape[0], shape[1], shape[2]);
+  let buffer = new Uint8Array(response, offset);
+  if (encoding === VolumeChunkEncoding.GZIP) {
+    buffer = inflate(buffer);
+  }
+  decodeRawChunk(chunk, buffer.buffer, Endianness.BIG, buffer.byteOffset, buffer.byteLength);
+}
+
+
+@registerSharedObject() export class PrecomputedVolumeChunkSource extends
+(WithParameters(VolumeChunkSource, VolumeChunkSourceParameters)) {
+  download(chunk: VolumeChunk, cancellationToken: CancellationToken) {
+    const {parameters} = this;
+    const {chunkGridPosition} = chunk;
+    const url =
+        `${parameters.url}/${chunkGridPosition[0]}/${chunkGridPosition[1]}/${chunkGridPosition[2]}`;
+    return sendHttpRequest(openHttpRequest(url), 'arraybuffer', cancellationToken)
+        .then(response => decodeChunk(chunk, response, parameters.encoding));
+  }
+}

src/neuroglancer/datasource/n5/base.ts
@@ -1,0 +1,27 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export enum VolumeChunkEncoding {
+  RAW,
+  GZIP,
+}
+
+export class VolumeChunkSourceParameters {
+  url: string;
+  encoding: VolumeChunkEncoding;
+
+  static RPC_ID = 'n5/VolumeChunkSource';
+}

src/neuroglancer/datasource/n5/frontend.ts
@@ -1,0 +1,206 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {AnnotationSource, makeDataBoundsBoundingBox} from 'neuroglancer/annotation';
+import {ChunkManager, WithParameters} from 'neuroglancer/chunk_manager/frontend';
+import {DataSource} from 'neuroglancer/datasource';
+import {VolumeChunkEncoding, VolumeChunkSourceParameters} from 'neuroglancer/datasource/n5/base';
+import {DataType, VolumeChunkSpecification, VolumeSourceOptions, VolumeType} from 'neuroglancer/sliceview/volume/base';
+import {MultiscaleVolumeChunkSource as GenericMultiscaleVolumeChunkSource, VolumeChunkSource} from 'neuroglancer/sliceview/volume/frontend';
+import {mat4, vec3} from 'neuroglancer/util/geom';
+import { openHttpRequest, parseSpecialUrl, sendHttpRequest, HttpError} from 'neuroglancer/util/http_request';
+import {parseArray, parseFixedLengthArray, verifyEnumString, verifyFinitePositiveFloat, verifyObject, verifyObjectProperty, verifyPositiveInt} from 'neuroglancer/util/json';
+
+class N5VolumeChunkSource extends
+(WithParameters(VolumeChunkSource, VolumeChunkSourceParameters)) {}
+
+export class MultiscaleVolumeChunkSource implements GenericMultiscaleVolumeChunkSource {
+  dataType: DataType;
+  volumeType: VolumeType;
+
+  get numChannels() {
+    return 1;
+  }
+
+  getMeshSource() {
+    return null;
+  }
+
+  baseScaleIndex: number;
+
+  constructor(
+      public chunkManager: ChunkManager, public url: string,
+      public topLevelMetadata: TopLevelMetadata, public scales: (ScaleMetadata|undefined)[]) {
+    let dataType: DataType|undefined;
+    let baseScaleIndex: number|undefined;
+    scales.forEach((scale, i) => {
+      if (scale === undefined) return;
+      if (baseScaleIndex === undefined) {
+        baseScaleIndex = i;
+      }
+      if (dataType !== undefined && scale.dataType !== dataType) {
+        throw new Error(`Scale s${i} has data type ${DataType[scale.dataType]} but expected ${
+            DataType[dataType]}.`);
+      }
+      dataType = scale.dataType;
+    });
+    if (dataType === undefined) {
+      throw new Error(`At least one scale must be specified.`);
+    }
+    this.dataType = dataType;
+    this.volumeType = VolumeType.IMAGE;
+    this.baseScaleIndex = baseScaleIndex!;
+  }
+
+  getSources(volumeSourceOptions: VolumeSourceOptions) {
+    const {topLevelMetadata} = this;
+    const sources: VolumeChunkSource[][] = [];
+    this.scales.forEach((scale, i) => {
+      if (scale === undefined) return;
+      sources.push(
+          VolumeChunkSpecification
+              .getDefaults({
+                voxelSize: vec3.multiply(
+                    vec3.create(), topLevelMetadata.pixelResolution, topLevelMetadata.scales[i]),
+                dataType: scale.dataType,
+                numChannels: 1,
+                upperVoxelBound: scale.size,
+                volumeType: this.volumeType,
+                chunkDataSizes: [scale.chunkSize],
+                volumeSourceOptions,
+              })
+              .map(spec => this.chunkManager.getChunkSource(N5VolumeChunkSource, {
+                spec,
+                parameters: {'url': `${this.url}/s${i}`, 'encoding': scale.encoding}
+              })));
+    });
+    return sources;
+  }
+
+  getStaticAnnotations() {
+    const {topLevelMetadata, baseScaleIndex} = this;
+    const annotationSet = new AnnotationSource(mat4.fromScaling(
+        mat4.create(),
+        vec3.multiply(
+            vec3.create(), topLevelMetadata.pixelResolution,
+            topLevelMetadata.scales[baseScaleIndex])));
+    annotationSet.readonly = true;
+    annotationSet.add(makeDataBoundsBoundingBox(vec3.create(), this.scales[baseScaleIndex]!.size));
+    return annotationSet;
+  }
+}
+
+const pixelResolutionUnits = new Map<string, number>([
+  ['mm', 1e6],
+  ['m', 1e9],
+  ['um', 1000],
+  ['nm', 1],
+]);
+
+class TopLevelMetadata {
+  pixelResolution: vec3;
+  scales: vec3[];
+  constructor(obj: any) {
+    verifyObject(obj);
+    verifyObjectProperty(obj, 'pixelResolution', resObj => {
+      verifyObject(resObj);
+      const unitScale = verifyObjectProperty(resObj, 'unit', x => {
+        const s = pixelResolutionUnits.get(x);
+        if (s === undefined) {
+          throw new Error(`Unsupported unit: ${JSON.stringify(x)}.`);
+        }
+        return s;
+      });
+      const dimensions = verifyObjectProperty(
+          resObj, 'dimensions',
+          x => parseFixedLengthArray(vec3.create(), x, verifyFinitePositiveFloat));
+      this.pixelResolution = vec3.scale(dimensions, dimensions, unitScale);
+    });
+    this.scales = verifyObjectProperty(
+        obj, 'scales',
+        scalesObj => parseArray(
+            scalesObj,
+            scaleObj => parseFixedLengthArray(vec3.create(), scaleObj, verifyFinitePositiveFloat)));
+  }
+}
+
+class ScaleMetadata {
+  dataType: DataType;
+  encoding: VolumeChunkEncoding;
+  size: vec3;
+  chunkSize: vec3;
+
+  constructor(obj: any) {
+    verifyObject(obj);
+    this.dataType = verifyObjectProperty(obj, 'dataType', x => verifyEnumString(x, DataType));
+    this.size = verifyObjectProperty(
+        obj, 'dimensions', x => parseFixedLengthArray(vec3.create(), x, verifyPositiveInt));
+    this.chunkSize = verifyObjectProperty(
+        obj, 'blockSize', x => parseFixedLengthArray(vec3.create(), x, verifyPositiveInt));
+    this.encoding =
+        verifyObjectProperty(obj, 'compressionType', x => verifyEnumString(x, VolumeChunkEncoding));
+  }
+}
+
+function getTopLevelMetadata(chunkManager: ChunkManager, url: string): Promise<TopLevelMetadata> {
+  return chunkManager.memoize.getUncounted(
+      {'type': 'n5:topLevelMetadata', url},
+      () => sendHttpRequest(openHttpRequest(url), 'json')
+                .then(response => new TopLevelMetadata(response)));
+}
+
+function getScaleMetadata(chunkManager: ChunkManager, url: string): Promise<ScaleMetadata> {
+  return chunkManager.memoize.getUncounted(
+      {'type': 'n5:scaleMetadata', url},
+      () => sendHttpRequest(openHttpRequest(url), 'json')
+                .then(response => new ScaleMetadata(response)));
+}
+
+function getAllScales(chunkManager: ChunkManager, url: string, topLevelMetadata: TopLevelMetadata):
+    Promise<(ScaleMetadata | undefined)[]> {
+  return Promise.all(topLevelMetadata.scales.map((_scale, i) => {
+    return getScaleMetadata(chunkManager, `${url}/s${i}/attributes.json`).catch(e => {
+      if (e instanceof HttpError && e.code === 404) {
+        return undefined;
+      }
+      throw e;
+    });
+  }));
+}
+
+export class N5DataSource extends DataSource {
+  get description() {
+    return 'N5 data source';
+  }
+  getVolume(chunkManager: ChunkManager, url: string) {
+    const [baseUrls, path] = parseSpecialUrl(url);
+    url = baseUrls[0] + path;
+    const m = url.match(/^(.*)\/(c[0-9]+)$/);
+    if (m === null) {
+      return Promise.reject(new Error('N5 url must end with the pattern "/c[0-9]+".'));
+    }
+    const topLevelMetadataUrl = `${m[1]}/attributes.json`;
+    return chunkManager.memoize.getUncounted(
+        {'type': 'n5:MultiscaleVolumeChunkSource', url},
+        () =>
+            getTopLevelMetadata(chunkManager, topLevelMetadataUrl)
+                .then(
+                    topLevelMetadata => getAllScales(chunkManager, url, topLevelMetadata)
+                                            .then(
+                                                scales => new MultiscaleVolumeChunkSource(
+                                                    chunkManager, url, topLevelMetadata, scales))));
+  }
+}

src/neuroglancer/datasource/n5/register_default.ts
@@ -1,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {registerProvider} from 'neuroglancer/datasource/default_provider';
+import {N5DataSource} from 'neuroglancer/datasource/n5/frontend';
+
+registerProvider('n5', () => new N5DataSource());

commit c63cf592811b8847d048a661a3f00ecebcbb6ab6

fix: Fix bug in decodeRawChunk for FLOAT32 data type
This bug was introduced by ce282b4e231eb220bd5201534ac518741ed8f3b7.

src/neuroglancer/sliceview/backend_chunk_decoders/raw.ts
@@ -48,7 +48,7 @@ export function decodeRawChunk(
       convertEndian32(data, endianness);
       break;
     case DataType.FLOAT32:
-      data = new Float32Array(response, byteOffset, byteLength / 2);
+      data = new Float32Array(response, byteOffset, byteLength / 4);
       convertEndian32(data, endianness);
       break;
     default: