ActiveBrainAtlas2/neuroglancer

commit 9c27e36c6e4d7fea8e7ef639e26ff41c7e82e1b4

fix(navigation_state): prevent spurious change notifications in RelativeDisplayScales and TrackableZoom
Previously, both RelativeDisplayScales and TrackableZoom triggered a change notification if the
coordinate space changed, even if the change did not affect the factors/zoom level, respectively.

Most notably, that led to spurious re-display of the DisplayDimensionsWidget.

src/neuroglancer/navigation_state.ts
@@ -497,8 +497,6 @@ export class LinkedOrientationState extends LinkedBase<OrientationState> {
 }
 
 export interface RelativeDisplayScales {
-  coordinateSpace: CoordinateSpace;
-
   /**
    * Array of length `coordinateSpace.rank` specifying scale factors on top of (will be multiply by)
    * `coordinateSpace.scales` to use for display purposes.  This allows non-uniform zooming.
@@ -509,10 +507,8 @@ export interface RelativeDisplayScales {
 export class TrackableRelativeDisplayScales extends RefCounted implements
     Trackable, WatchableValueInterface<RelativeDisplayScales> {
   changed = new NullarySignal();
-  private value_: RelativeDisplayScales = {
-    coordinateSpace: emptyInvalidCoordinateSpace,
-    factors: new Float64Array(0)
-  };
+  private curCoordinateSpace = emptyInvalidCoordinateSpace;
+  private value_: RelativeDisplayScales = {factors: new Float64Array(0)};
   constructor(public coordinateSpace: WatchableValueInterface<CoordinateSpace>) {
     super();
     this.registerDisposer(coordinateSpace.changed.add(() => this.update()));
@@ -524,7 +520,8 @@ export class TrackableRelativeDisplayScales extends RefCounted implements
   }
 
   reset() {
-    this.value_ = {coordinateSpace: emptyInvalidCoordinateSpace, factors: new Float64Array(0)};
+    this.value_ = {factors: new Float64Array(0)};
+    this.curCoordinateSpace = emptyInvalidCoordinateSpace;
     this.changed.dispatch();
   }
 
@@ -532,7 +529,8 @@ export class TrackableRelativeDisplayScales extends RefCounted implements
     const json: any = {};
     let nonEmpty = false;
     const {value} = this;
-    const {coordinateSpace: {names, rank}, factors} = value;
+    const {factors} = value;
+    const {names, rank} = this.curCoordinateSpace;
     for (let i = 0; i < rank; ++i) {
       const factor = factors[i];
       if (factor === 1) continue;
@@ -555,22 +553,25 @@ export class TrackableRelativeDisplayScales extends RefCounted implements
             obj, names[i], x => x === undefined ? 1 : verifyFinitePositiveFloat(x));
       }
     }
-    this.value_ = {coordinateSpace, factors};
+    this.value_ = {factors};
+    this.curCoordinateSpace = coordinateSpace;
     this.changed.dispatch();
   }
 
   setFactors(factors: Float64Array) {
     const {coordinateSpace: {value: coordinateSpace}} = this;
     if (factors.length !== coordinateSpace.rank) return;
-    this.value_ = {coordinateSpace, factors};
+    this.value_ = {factors};
+    this.curCoordinateSpace = coordinateSpace;
     this.changed.dispatch();
   }
 
   private update() {
     const {coordinateSpace: {value: coordinateSpace}} = this;
     let value = this.value_;
-    if (value.coordinateSpace === coordinateSpace) return value;
-    const {ids: oldDimensionIds} = value.coordinateSpace;
+    const {curCoordinateSpace} = this;
+    if (curCoordinateSpace === coordinateSpace) return value;
+    const {ids: oldDimensionIds} = curCoordinateSpace;
     const {ids: newDimensionIds, rank} = coordinateSpace;
     const oldFactors = value.factors;
     const newFactors = new Float64Array(rank);
@@ -581,7 +582,9 @@ export class TrackableRelativeDisplayScales extends RefCounted implements
       if (oldIndex === -1) continue;
       newFactors[i] = oldFactors[oldIndex];
     }
-    value = this.value_ = {coordinateSpace, factors: newFactors};
+    if (arraysEqual(newFactors, oldFactors)) return value;
+    value = this.value_ = {factors: newFactors};
+    this.curCoordinateSpace = coordinateSpace;
     this.changed.dispatch();
     return value;
   }
@@ -613,7 +616,8 @@ export class LinkedRelativeDisplayScales extends LinkedBase<TrackableRelativeDis
         assign: (target, source) => target.assign(source),
         difference:
             (a, b) => {
-              const {factors: fa, coordinateSpace} = a.value;
+              const {factors: fa} = a.value;
+              const coordinateSpace = a.coordinateSpace.value;
               const fb = b.value.factors;
               return {
                 coordinateSpace,
@@ -677,6 +681,13 @@ export interface DisplayDimensionRenderInfo {
   displayDimensionUnits: readonly string[];
 
   /**
+   * Scale corresponding to each dimension in `displayDimensionIndices`.
+   * `displayDimensionScales[i]` is equal to `coordinateSpace.scales[displayDimensionIndices[i]]`,
+   * or `1` if `displayDimensionIndices[i] == -1`.
+   */
+  displayDimensionScales: Float64Array;
+
+  /**
    * Physical scale corresponding to the canonical voxel.  Equal to minimum of
    * `voxelPhysicalScales.slice(0, rank)`, or `1` if `rank == 0`.
    */
@@ -691,17 +702,19 @@ export interface DisplayDimensionRenderInfo {
 }
 
 function getDisplayDimensionRenderInfo(
-    displayDimensions: DisplayDimensions,
+    coordinateSpace: CoordinateSpace, displayDimensions: DisplayDimensions,
     relativeDisplayScales: RelativeDisplayScales): DisplayDimensionRenderInfo {
-  const {rank: globalRank, names: globalDimensionNames, units} = relativeDisplayScales.coordinateSpace;
+  const {rank: globalRank, names: globalDimensionNames, units} = coordinateSpace;
   const {displayRank, displayDimensionIndices} = displayDimensions;
   const canonicalVoxelFactors = new Float64Array(3);
   let voxelPhysicalScales = new Float64Array(3);
   let canonicalVoxelPhysicalSize: number;
-  const {coordinateSpace, factors} = relativeDisplayScales;
+  const {factors} = relativeDisplayScales;
   const displayDimensionUnits = new Array<string>(3);
+  const displayDimensionScales = new Float64Array(3);
   canonicalVoxelFactors.fill(1);
   voxelPhysicalScales.fill(1);
+  displayDimensionScales.fill(1);
   displayDimensionUnits.fill('');
   if (displayRank === 0) {
     canonicalVoxelPhysicalSize = 1;
@@ -713,6 +726,7 @@ function getDisplayDimensionRenderInfo(
       const s = voxelPhysicalScales[i] = factors[dim] * scales[dim];
       canonicalVoxelPhysicalSize = Math.min(canonicalVoxelPhysicalSize, s);
       displayDimensionUnits[i] = units[dim];
+      displayDimensionScales[i] = scales[dim];
     }
     for (let i = 0; i < displayRank; ++i) {
       canonicalVoxelFactors[i] = voxelPhysicalScales[i] / canonicalVoxelPhysicalSize;
@@ -724,6 +738,7 @@ function getDisplayDimensionRenderInfo(
     displayRank,
     displayDimensionIndices,
     displayDimensionUnits,
+    displayDimensionScales,
     canonicalVoxelFactors,
     voxelPhysicalScales,
     canonicalVoxelPhysicalSize,
@@ -737,28 +752,34 @@ export function displayDimensionRenderInfosEqual(
       arraysEqual(a.canonicalVoxelFactors, b.canonicalVoxelFactors) &&
       arraysEqual(a.voxelPhysicalScales, b.voxelPhysicalScales) &&
       a.canonicalVoxelPhysicalSize === b.canonicalVoxelPhysicalSize &&
-      arraysEqual(a.displayDimensionUnits, b.displayDimensionUnits);
+      arraysEqual(a.displayDimensionUnits, b.displayDimensionUnits) &&
+      arraysEqual(a.displayDimensionScales, b.displayDimensionScales);
 }
 
 export class WatchableDisplayDimensionRenderInfo extends RefCounted {
   changed = new NullarySignal();
   private curRelativeDisplayScales: RelativeDisplayScales = this.relativeDisplayScales.value;
   private curDisplayDimensions: DisplayDimensions = this.displayDimensions.value;
-  private value_: DisplayDimensionRenderInfo =
-      getDisplayDimensionRenderInfo(this.curDisplayDimensions, this.curRelativeDisplayScales);
+  private curCoordinateSpace: CoordinateSpace = this.relativeDisplayScales.coordinateSpace.value;
+  private value_: DisplayDimensionRenderInfo = getDisplayDimensionRenderInfo(
+      this.curCoordinateSpace, this.curDisplayDimensions, this.curRelativeDisplayScales);
   get value() {
     const {
-      relativeDisplayScales: {value: relativeDisplayScales},
+      relativeDisplayScales:
+          {value: relativeDisplayScales, coordinateSpace: {value: coordinateSpace}},
       displayDimensions: {value: displayDimensions},
       curRelativeDisplayScales,
       curDisplayDimensions,
+      curCoordinateSpace,
     } = this;
     let value = this.value_;
     if (curRelativeDisplayScales !== relativeDisplayScales ||
-        curDisplayDimensions !== displayDimensions) {
+        curDisplayDimensions !== displayDimensions || curCoordinateSpace !== coordinateSpace) {
       this.curRelativeDisplayScales = relativeDisplayScales;
       this.curDisplayDimensions = displayDimensions;
-      const newValue = getDisplayDimensionRenderInfo(displayDimensions, relativeDisplayScales);
+      this.curCoordinateSpace = coordinateSpace;
+      const newValue =
+          getDisplayDimensionRenderInfo(coordinateSpace, displayDimensions, relativeDisplayScales);
       if (!displayDimensionRenderInfosEqual(value, newValue)) {
         this.value_ = value = newValue;
         this.changed.dispatch();
@@ -1203,7 +1224,6 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
                                                            WatchableValueInterface<number> {
   readonly changed = new NullarySignal();
   private curCanonicalVoxelPhysicalSize = 0;
-  private curCoordinateSpace: CoordinateSpace = emptyInvalidCoordinateSpace;
   private value_: number = Number.NaN;
   protected legacyValue_: number = Number.NaN;
 
@@ -1243,9 +1263,9 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
    */
   set legacyValue(value: number) {
     if (Object.is(value, this.legacyValue_)) return;
-    this.curCoordinateSpace = emptyInvalidCoordinateSpace;
     this.value_ = Number.NaN;
     this.legacyValue_ = value;
+    this.curCanonicalVoxelPhysicalSize = 0;
     this.changed.dispatch();
   }
 
@@ -1270,13 +1290,10 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
       }
     } = this;
     const {curCanonicalVoxelPhysicalSize} = this;
-    const {curCoordinateSpace} = this;
-    if (canonicalVoxelPhysicalSize === curCanonicalVoxelPhysicalSize &&
-        curCoordinateSpace === coordinateSpace) {
+    if (canonicalVoxelPhysicalSize === curCanonicalVoxelPhysicalSize) {
       return;
     }
     this.curCanonicalVoxelPhysicalSize = canonicalVoxelPhysicalSize;
-    this.curCoordinateSpace = coordinateSpace;
     if (!Number.isNaN(value_)) {
       if (curCanonicalVoxelPhysicalSize !== 0) {
         this.value_ = value_ * (curCanonicalVoxelPhysicalSize / canonicalVoxelPhysicalSize);
@@ -1298,7 +1315,6 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
 
   restoreState(obj: any) {
     this.curCanonicalVoxelPhysicalSize = 0;
-    this.curCoordinateSpace = emptyInvalidCoordinateSpace;
     this.legacyValue_ = Number.NaN;
     if (obj === undefined) {
       this.value_ = Number.NaN;
@@ -1310,7 +1326,6 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
 
   reset() {
     this.curCanonicalVoxelPhysicalSize = 0;
-    this.curCoordinateSpace = emptyInvalidCoordinateSpace;
     this.value_ = Number.NaN;
     this.legacyValue_ = Number.NaN;
     this.changed.dispatch();

src/neuroglancer/widget/display_dimensions_widget.ts
@@ -335,7 +335,7 @@ export class DisplayDimensionsWidget extends RefCounted {
         canonicalVoxelPhysicalSize,
       } = displayDimensionRenderInfoValue;
       const widgets: DepthWidget[] = [];
-      
+
       const updateView = () => {
         relativeCheckbox.checked = this.depthRange.value < 0;
         let rangeValue = this.depthRange.value;
@@ -359,7 +359,7 @@ export class DisplayDimensionsWidget extends RefCounted {
         this.depthRange.value = value;
         return true;
       };
-      
+
       for (let i = 0; i < displayRank; ++i) {
         const dim = displayDimensionIndices[i];
         const name = globalDimensionNames[dim];
@@ -485,21 +485,23 @@ export class DisplayDimensionsWidget extends RefCounted {
     const {
       displayDimensionIndices,
       canonicalVoxelFactors,
+      displayDimensionUnits,
+      displayDimensionScales,
+      globalDimensionNames,
     } = this.displayDimensionRenderInfo.value;
-    const {factors, coordinateSpace: {names, units, scales}} = this.relativeDisplayScales.value;
+    const {factors} = this.relativeDisplayScales.value;
     this.defaultCheckbox.checked = isDefault;
     const zoom = this.zoom.value;
     // Check if all units and factors are the same.
     const firstDim = displayDimensionIndices[0];
     let singleScale = true;
     if (firstDim !== -1) {
-      const unit = units[firstDim];
+      const unit = displayDimensionUnits[0];
       const factor = factors[firstDim];
       for (let i = 1; i < 3; ++i) {
         const dim = displayDimensionIndices[i];
         if (dim === -1) continue;
-        if (units[dim] !== unit ||
-            factors[dim] !== factor) {
+        if (displayDimensionUnits[i] !== unit || factors[dim] !== factor) {
           singleScale = false;
           break;
         }
@@ -515,11 +517,11 @@ export class DisplayDimensionsWidget extends RefCounted {
         dimElements.scale.textContent = '';
         dimElements.scaleFactor.value = '';
       } else {
-        dimElements.name.value = names[dim];
-        const totalScale = scales[dim] * zoom / canonicalVoxelFactors[i];
+        dimElements.name.value = globalDimensionNames[dim];
+        const totalScale = displayDimensionScales[i] * zoom / canonicalVoxelFactors[i];
         if (i === 0 || !singleScale) {
-          const formattedScale =
-              formatScaleWithUnitAsString(totalScale, units[dim], {precision: 2, elide1: false});
+          const formattedScale = formatScaleWithUnitAsString(
+              totalScale, displayDimensionUnits[i], {precision: 2, elide1: false});
           dimElements.scale.textContent = `${formattedScale}/${this.displayUnit}`;
         } else {
           dimElements.scale.textContent = '';

src/neuroglancer/widget/scale_bar.ts
@@ -29,7 +29,7 @@
  * understand.
  */
 
-import { DisplayDimensionRenderInfo, RelativeDisplayScales} from 'neuroglancer/navigation_state';
+import {DisplayDimensionRenderInfo, RelativeDisplayScales} from 'neuroglancer/navigation_state';
 import {TrackableValue} from 'neuroglancer/trackable_value';
 import {RefCounted} from 'neuroglancer/util/disposable';
 import {verifyFloat, verifyObjectProperty, verifyString} from 'neuroglancer/util/json';
@@ -257,12 +257,15 @@ export class MultipleScaleBarTextures extends RefCounted {
       options: ScaleBarOptions) {
     const {scaleBars} = this;
     const {
-        displayRank,
-        displayDimensionIndices,
-        canonicalVoxelFactors,
+      displayRank,
+      displayDimensionIndices,
+      canonicalVoxelFactors,
+      globalDimensionNames,
+      displayDimensionUnits,
+      displayDimensionScales,
     } = displayDimensionRenderInfo;
 
-    const {factors, coordinateSpace: {names, scales, units}} = relativeDisplayScales;
+    const {factors} = relativeDisplayScales;
 
     const targetLengthInPixels = Math.min(
         options.maxWidthFraction * viewportWidth, options.maxWidthInPixels * options.scaleFactor);
@@ -271,7 +274,7 @@ export class MultipleScaleBarTextures extends RefCounted {
 
     for (let i = 0; i < displayRank; ++i) {
       const dim = displayDimensionIndices[i];
-      const unit = units[dim];
+      const unit = displayDimensionUnits[i];
       const factor = factors[dim];
       let barIndex;
       let scaleBar: ScaleBarTexture;
@@ -292,9 +295,9 @@ export class MultipleScaleBarTextures extends RefCounted {
         scaleBarDimensions.physicalBaseUnit = unit;
         scaleBarDimensions.targetLengthInPixels = targetLengthInPixels;
         scaleBarDimensions.physicalSizePerPixel =
-            scales[dim] * effectiveZoom / canonicalVoxelFactors[i];
+            displayDimensionScales[i] * effectiveZoom / canonicalVoxelFactors[i];
       }
-      scaleBar!.label += `${names[dim]} `;
+      scaleBar!.label += `${globalDimensionNames[dim]} `;
     }
 
     const {gl, scaleBarCopyHelper} = this;

commit 3d74fd85b1224a1357acb92fec5dc2c6b1578bf6

fix(navigation_state): fix bug whereby zoom factor could be left unset
9c27e36c6e4d7fea8e7ef639e26ff41c7e82e1b4 introduced a regression such that legacy cross-section zoom
factors were not properly handled.

src/neuroglancer/navigation_state.ts
@@ -1278,6 +1278,9 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
     this.registerDisposer(displayDimensionRenderInfo);
     this.registerDisposer(
         displayDimensionRenderInfo.changed.add(() => this.handleCoordinateSpaceChanged()));
+    this.registerDisposer(
+        displayDimensionRenderInfo.relativeDisplayScales.coordinateSpace.changed.add(
+            () => this.handleCoordinateSpaceChanged()));
     this.handleCoordinateSpaceChanged();
   }
 
@@ -1290,18 +1293,21 @@ abstract class TrackableZoom extends RefCounted implements Trackable,
       }
     } = this;
     const {curCanonicalVoxelPhysicalSize} = this;
-    if (canonicalVoxelPhysicalSize === curCanonicalVoxelPhysicalSize) {
+    if (!Number.isNaN(value_) && canonicalVoxelPhysicalSize === curCanonicalVoxelPhysicalSize) {
       return;
     }
-    this.curCanonicalVoxelPhysicalSize = canonicalVoxelPhysicalSize;
     if (!Number.isNaN(value_)) {
       if (curCanonicalVoxelPhysicalSize !== 0) {
         this.value_ = value_ * (curCanonicalVoxelPhysicalSize / canonicalVoxelPhysicalSize);
+        this.curCanonicalVoxelPhysicalSize = canonicalVoxelPhysicalSize;
         this.changed.dispatch();
       }
       return;
     }
-    if (!coordinateSpace.valid || canonicalVoxelPhysicalSize === 0) return;
+    if (!coordinateSpace.valid || canonicalVoxelPhysicalSize === 0) {
+      return;
+    }
+    this.curCanonicalVoxelPhysicalSize = canonicalVoxelPhysicalSize;
     this.value_ = this.getDefaultValue();
     this.changed.dispatch();
   }
@@ -1543,7 +1549,7 @@ export class NavigationState<Zoom extends TrackableZoomInterface = TrackableZoom
   }
 
   get valid() {
-    return this.pose.valid;
+    return this.pose.valid && !Number.isNaN(this.zoomFactor.value);
   }
 
   zoomBy(factor: number) {