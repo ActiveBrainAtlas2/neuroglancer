ActiveBrainAtlas2/neuroglancer

commit 949e42c936f7d9cf784408fc865c65aa4da9492a

feat: custom annotation shaders + precomputed annotation support
This adds several new annotation features:
- support for user-defined properties associated with each annotation
- custom shaders (which can access annotation properties, if any)
- support for multiple segment id relationships, rather than just a single one
- precomputed annotation format with spatial indexing support

src/neuroglancer/annotation/base.ts
@@ -14,19 +14,124 @@
  * limitations under the License.
  */
 
-import {SliceViewChunkSpecification} from 'neuroglancer/sliceview/base';
+import {DisplayDimensionRenderInfo} from 'neuroglancer/navigation_state';
+import {forEachVisibleVolumetricChunk, SliceViewChunkSource, SliceViewChunkSpecification, SliceViewRenderLayer, TransformedSource} from 'neuroglancer/sliceview/base';
+import {getViewFrustrumVolume, mat3, mat3FromMat4, mat4, prod3} from 'neuroglancer/util/geom';
 
 export const ANNOTATION_METADATA_CHUNK_SOURCE_RPC_ID = 'annotation.MetadataChunkSource';
 export const ANNOTATION_GEOMETRY_CHUNK_SOURCE_RPC_ID = 'annotation.GeometryChunkSource';
-export const ANNOTATION_SUBSET_GEOMETRY_CHUNK_SOURCE_RPC_ID = 'annotation.SubsetGeometryChunkSource';
+export const ANNOTATION_SUBSET_GEOMETRY_CHUNK_SOURCE_RPC_ID =
+    'annotation.SubsetGeometryChunkSource';
 export const ANNOTATION_REFERENCE_ADD_RPC_ID = 'annotation.reference.add';
 export const ANNOTATION_REFERENCE_DELETE_RPC_ID = 'annotation.reference.delete';
 export const ANNOTATION_COMMIT_UPDATE_RPC_ID = 'annotation.commit';
 export const ANNOTATION_COMMIT_UPDATE_RESULT_RPC_ID = 'annotation.commit';
 
-export interface AnnotationGeometryChunkSpecification extends SliceViewChunkSpecification {}
+export interface AnnotationGeometryChunkSpecification extends SliceViewChunkSpecification {
+  limit: number;
+}
 
-export const ANNOTATION_PERSPECTIVE_RENDER_LAYER_RPC_ID = 'annotation/PerspectiveRenderLayer';
+export const ANNOTATION_SPATIALLY_INDEXED_RENDER_LAYER_RPC_ID =
+    'annotation/SpatiallyIndexedRenderLayer';
+export const ANNOTATION_PERSPECTIVE_RENDER_LAYER_UPDATE_SOURCES_RPC_ID =
+    'annotation/PerspectiveRenderLayer:updateSources';
 export const ANNOTATION_RENDER_LAYER_RPC_ID = 'annotation/RenderLayer';
 export const ANNOTATION_RENDER_LAYER_UPDATE_SEGMENTATION_RPC_ID =
     'annotation/RenderLayer.updateSegmentation';
+
+const tempMat3 = mat3.create();
+
+export function
+forEachVisibleAnnotationChunk<RLayer extends SliceViewRenderLayer, Source extends
+                                  SliceViewChunkSource<AnnotationGeometryChunkSpecification>,
+                                  Transformed extends TransformedSource<RLayer, Source>>(
+    globalPosition: Float32Array, localPosition: Float32Array, projectionMatrix: mat4,
+    viewMatrix: mat4, viewProjection: mat4, viewportWidth: number, viewportHeight: number,
+    displayDimensionRenderInfo: DisplayDimensionRenderInfo, renderScaleTarget: number,
+    transformedSources: readonly Transformed[],
+    beginScale: (source: Transformed, index: number) => void,
+    callback: (
+        source: Transformed, index: number, maxCount: number, physicalSpacing: number,
+        pixelSpacing: number) => void) {
+  const {voxelPhysicalScales} = displayDimensionRenderInfo;
+  const viewDet = mat3.determinant(mat3FromMat4(tempMat3, viewMatrix));
+  const canonicalToPhysicalScale = prod3(voxelPhysicalScales);
+  const viewFrustrumVolume =
+      getViewFrustrumVolume(projectionMatrix) / viewDet * canonicalToPhysicalScale;
+
+  if (transformedSources.length === 0) return;
+  const baseSource = transformedSources[0];
+  let sourceVolume = baseSource.chunkLayout.detTransform * canonicalToPhysicalScale;
+  const {lowerClipDisplayBound, upperClipDisplayBound} = baseSource;
+  for (let i = 0; i < 3; ++i) {
+    sourceVolume *= (upperClipDisplayBound[i] - lowerClipDisplayBound[i]);
+  }
+
+  const effectiveVolume = Math.min(sourceVolume, viewFrustrumVolume);
+  const viewportArea = viewportWidth * viewportHeight;
+  const targetNumAnnotations = viewportArea / (renderScaleTarget ** 2);
+  const physicalDensityTarget = targetNumAnnotations / effectiveVolume;
+
+  // Target density in annotations per physical volume.
+  let totalPhysicalDensity = 0;
+  for (let scaleIndex = transformedSources.length - 1;
+       scaleIndex >= 0 && totalPhysicalDensity < physicalDensityTarget; --scaleIndex) {
+    const transformedSource = transformedSources[scaleIndex];
+    const spec = transformedSource.source.spec as AnnotationGeometryChunkSpecification;
+    const {chunkLayout} = transformedSource;
+    const physicalVolume =
+      prod3(chunkLayout.size) * chunkLayout.detTransform * canonicalToPhysicalScale;
+    const {limit, rank} = spec;
+    const {nonDisplayLowerClipBound, nonDisplayUpperClipBound} = transformedSource;
+    let sliceFraction = 1;
+    for (let i = 0; i < rank; ++i) {
+      const b = (nonDisplayUpperClipBound[i] - nonDisplayLowerClipBound[i]);
+      if (Number.isFinite(b)) sliceFraction /= b;
+    }
+    const physicalDensity = limit * sliceFraction / physicalVolume;
+
+
+    // pixelSize = baseScale * w
+    // totalPixelDensity = totalPhysicalDensity * (pixelSize ** 3)
+
+    // If totalPixelDensity >= pixelDensityTarget, we can stop.
+
+    // i.e.   totalPhysicalDensity * (pixelSize ** 3) >= pixelDensityTarget
+    // i.e.   totalPhysicalDensity * ((baseScale * w) ** 3) >= pixelDensityTarget
+    // i.e.   totalPhysicalDensity * baseScale**3 * w**3 >= pixelDensityTarget
+    // i.e.   w**3 >= pixelDensityTarget / (totalPhysicalDensity * baseScale**3)
+    // i.e.   w >= Math.pow(pixelDensityTarget / (totalPhysicalDensity * baseScale**3), 1/3)
+
+    // let wLimit: number;
+    // if (totalPhysicalDensity === 0) {
+    //   wLimit = Number.POSITIVE_INFINITY;
+    // } else {
+    //   wLimit = Math.pow(pixelDensityTarget / (totalPhysicalDensity * baseScale ** 3), 1 / 3);
+    // }
+
+    let firstChunk = true;
+    const newTotalPhysicalDensity = totalPhysicalDensity + physicalDensity;
+    const totalPhysicalSpacing = Math.pow(1 / newTotalPhysicalDensity, 1 / 3);
+    const totalPixelSpacing = Math.sqrt(viewportArea / (newTotalPhysicalDensity * effectiveVolume));
+    // const newTotalPhysicalDensity = totalPhysicalDensity + spec.limit / physicalVolume;
+    const maxCount =
+        (physicalDensityTarget - totalPhysicalDensity) * physicalVolume / sliceFraction;
+    // const totalPhysicalSpacing = Math.pow(1 / newTotalPhysicalDensity, 1 / 3);
+
+    forEachVisibleVolumetricChunk(
+        globalPosition, localPosition, viewProjection, transformedSource, Number.POSITIVE_INFINITY,
+        () => {
+          // const pixelSize = baseScale * minW;
+          // const pixelSize3 = pixelSize ** 3;
+          // const pixelVolume = physicalVolume / pixelSize3;
+          // const totalPixelDensity = totalPhysicalDensity * pixelSize3;
+          if (firstChunk) {
+            beginScale(transformedSource, scaleIndex);
+            firstChunk = false;
+          }
+          callback(
+              transformedSource, scaleIndex, maxCount, totalPhysicalSpacing, totalPixelSpacing);
+        });
+    totalPhysicalDensity = newTotalPhysicalDensity;
+  }
+}

commit 4aea79047f44e60a0175260b3b94b17d8783557c

fix(annotations): fix non-local annotation write support
Support for writing to non-local annotation sources was broken by
949e42c936f7d9cf784408fc865c65aa4da9492a.

This change fixes the regressions, and also properly implements updating spatially-indexed
annotation sources.

src/neuroglancer/annotation/base.ts
@@ -28,6 +28,16 @@ export const ANNOTATION_COMMIT_UPDATE_RPC_ID = 'annotation.commit';
 export const ANNOTATION_COMMIT_UPDATE_RESULT_RPC_ID = 'annotation.commit';
 
 export interface AnnotationGeometryChunkSpecification extends SliceViewChunkSpecification {
+  /**
+   * Must equal the `chunkToMultiscaleTransform` in the `SliceViewSingleResolutionSource`.
+   */
+  chunkToMultiscaleTransform: Float32Array;
+
+  /**
+   * Specifies the maximum density of annotations provided by this chunk source, as `limit` per the
+   * chunk volume.  The higher the value, the sooner chunks from this source will be subsampled.  To
+   * disable subsampling completely, set `limit` to 0.
+   */
   limit: number;
 }
 

src/neuroglancer/annotation/frontend_source.ts
@@ -28,6 +28,7 @@ import {NullarySignal, Signal} from 'neuroglancer/util/signal';
 import {Buffer} from 'neuroglancer/webgl/buffer';
 import {GL} from 'neuroglancer/webgl/context';
 import {registerRPC, registerSharedObjectOwner, RPC, SharedObject} from 'neuroglancer/worker_rpc';
+import * as matrix from 'neuroglancer/util/matrix';
 
 export interface AnnotationGeometryChunkSourceOptions extends SliceViewChunkSourceOptions {
   spec: AnnotationGeometryChunkSpecification;
@@ -125,10 +126,32 @@ export class AnnotationGeometryChunkSource extends
   parent: Borrowed<MultiscaleAnnotationSource>;
   immediateChunkUpdates = true;
 
+  /**
+   * Transforms positions in the MultiscaleAnnotationSource coordinate space to grid cell
+   * coordinates.  Equal to the inverse of `this.spec.chunkToMultiscaleTransform`, with rows divided
+   * by `this.spec.chunkDataSize`.
+   */
+  multiscaleToChunkTransform: Float32Array;
+
   constructor(chunkManager: Borrowed<ChunkManager>, options: AnnotationGeometryChunkSourceOptions) {
     super(chunkManager, options);
-    this.parent = options.parent;
-    // FIXME: register with parent
+    const parent = this.parent = options.parent;
+    parent.spatiallyIndexedSources.add(this);
+    const {rank, chunkDataSize} = this.spec;
+    const multiscaleToChunkTransform = this.multiscaleToChunkTransform = new Float32Array((rank + 1) ** 2);
+    matrix.inverse(
+        multiscaleToChunkTransform, rank + 1, this.spec.chunkToMultiscaleTransform, rank + 1,
+        rank + 1);
+    for (let i = 0; i < rank; ++i) {
+      for (let j = 0; j < rank + 1; ++j) {
+        multiscaleToChunkTransform[(rank + 1) * j + i] /= chunkDataSize[i];
+      }
+    }
+  }
+
+  disposed() {
+    this.parent.spatiallyIndexedSources.delete(this);
+    super.disposed();
   }
 
   initializeCounterpart(rpc: RPC, options: any) {
@@ -204,7 +227,7 @@ export class AnnotationMetadataChunkSource extends ChunkSource {
   }
 }
 
-function updateAnnotation(
+export function updateAnnotation(
     chunk: AnnotationGeometryData, annotation: Annotation,
     propertySerializer: AnnotationPropertySerializer) {
   // Find insertion point.
@@ -236,7 +259,9 @@ function updateAnnotation(
   } else {
     offset = serializedAnnotations.typeToOffset[type] + numBytes * index;
   }
-  const dv = new DataView(serializedAnnotations.data);
+  const dv = new DataView(
+      serializedAnnotations.data.buffer, serializedAnnotations.data.byteOffset,
+      serializedAnnotations.data.byteLength);
   let bufferOffset = serializedAnnotations.typeToOffset[type] + index * numBytes;
   const isLittleEndian = ENDIANNESS === Endianness.LITTLE;
   handler.serialize(dv, bufferOffset, isLittleEndian, rank, annotation);
@@ -245,7 +270,7 @@ function updateAnnotation(
   chunk.bufferValid = false;
 }
 
-function deleteAnnotation(
+export function deleteAnnotation(
     chunk: AnnotationGeometryData, type: AnnotationType, id: AnnotationId,
     propertySerializer: AnnotationPropertySerializer): boolean {
   const {serializedAnnotations} = chunk;
@@ -260,10 +285,9 @@ function deleteAnnotation(
   const numGeometryBytes = handler.serializedBytes(rank);
   const numBytes = numGeometryBytes + propertySerializer.serializedBytes;
   ids.splice(index, 1);
-  const count = idMap.size;
   idMap.delete(id);
-  for (let i = index + 1; i < count; ++i) {
-    idMap.set(ids[i], i - 1);
+  for (let i = index, count = ids.length; i < count; ++i) {
+    idMap.set(ids[i], i);
   }
   const {typeToOffset} = serializedAnnotations;
   const offset = typeToOffset[type] + numBytes * index;
@@ -303,7 +327,7 @@ interface LocalUpdateUndoState {
   type: AnnotationType;
 }
 
-function makeTemporaryChunk() {
+export function makeTemporaryChunk() {
   const typeToIds: string[][] = [];
   const typeToOffset: number[] = [];
   const typeToIdMaps: Map<string, number>[] = [];
@@ -324,6 +348,7 @@ export class MultiscaleAnnotationSource extends SharedObject implements
   metadataChunkSource =
       this.registerDisposer(new AnnotationMetadataChunkSource(this.chunkManager, this));
   segmentFilteredSources: Owned<AnnotationSubsetGeometryChunkSource>[];
+  spatiallyIndexedSources = new Set<Borrowed<AnnotationGeometryChunkSource>>();
   rank: number;
   readonly relationships: readonly string[];
   readonly properties: Readonly<AnnotationPropertySpec>[];
@@ -501,22 +526,81 @@ export class MultiscaleAnnotationSource extends SharedObject implements
   private forEachPossibleChunk(
       annotation: Annotation,
       callback: (chunk: AnnotationGeometryChunk|AnnotationSubsetGeometryChunk) => void) {
-    // FIXME: also handle spatially-indexed chunks
     annotation;
     const {relatedSegments} = annotation;
-    if (relatedSegments === undefined) return;
-    const numRelationships = relatedSegments.length;
-    const {segmentFilteredSources} = this;
-    for (let i = 0; i < numRelationships; ++i) {
-      const segments = relatedSegments[i];
-      if (segments === undefined) return;
-      const source = segmentFilteredSources[i];
-      for (const segment of segments) {
-        const chunk = source.chunks.get(getObjectKey(segment));
-        if (chunk === undefined) {
-          continue;
+    if (relatedSegments !== undefined) {
+      const numRelationships = relatedSegments.length;
+      const {segmentFilteredSources} = this;
+      for (let i = 0; i < numRelationships; ++i) {
+        const segments = relatedSegments[i];
+        if (segments === undefined) return;
+        const source = segmentFilteredSources[i];
+        for (const segment of segments) {
+          const chunk = source.chunks.get(getObjectKey(segment));
+          if (chunk === undefined) {
+            continue;
+          }
+          callback(chunk);
         }
-        callback(chunk);
+      }
+    }
+    const {rank} = this;
+    const tempLower = new Float32Array(rank);
+    const tempUpper = new Float32Array(rank);
+    const tempChunk = new Float32Array(rank);
+    for (const source of this.spatiallyIndexedSources) {
+      switch (annotation.type) {
+        case AnnotationType.POINT:
+          matrix.transformPoint(
+            tempLower, source.multiscaleToChunkTransform, rank + 1, annotation.point, rank);
+          tempUpper.set(tempLower);
+          break;
+        case AnnotationType.LINE:
+        case AnnotationType.AXIS_ALIGNED_BOUNDING_BOX:
+          matrix.transformPoint(
+            tempLower, source.multiscaleToChunkTransform, rank + 1, annotation.pointA, rank);
+          matrix.transformPoint(
+            tempUpper, source.multiscaleToChunkTransform, rank + 1, annotation.pointB, rank);
+          break;
+        case AnnotationType.ELLIPSOID:
+          matrix.transformPoint(
+            tempLower, source.multiscaleToChunkTransform, rank + 1, annotation.center, rank);
+          matrix.transformVector(
+            tempUpper, source.multiscaleToChunkTransform, rank + 1, annotation.radii, rank);
+          for (let i = 0; i < rank; ++i) {
+            const c = tempLower[i];
+            const r = tempUpper[i];
+            tempLower[i] = c - r;
+            tempUpper[i] = c + r;
+          }
+          break;
+      }
+      let totalChunks = 1;
+      for (let i = 0; i < rank; ++i) {
+        const a = tempLower[i];
+        const b = tempUpper[i];
+        const lower = Math.min(a, b);
+        const upper = Math.max(a, b);
+        // In the case that the point lies directly on a boundary, ensure it is included in both
+        // chunks, since we don't know how the datasource handles this case.
+        tempLower[i] = Math.ceil(lower - 1);
+        tempUpper[i] = Math.floor(upper + 1);
+        totalChunks *= (tempUpper[i] - tempLower[i]);
+      }
+      const {chunks} = source;
+      for (let chunkIndex = 0; chunkIndex < totalChunks; ++chunkIndex) {
+        let remainder = chunkIndex;
+        for (let i = 0; i < rank; ++i) {
+          const lower = tempLower[i];
+          const upper = tempUpper[i];
+          const size = upper - lower;
+          const x = tempChunk[i] = remainder % size;
+          remainder = (remainder - x) / size;
+        }
+        const chunk = chunks.get(tempChunk.join());
+        if (chunk !== undefined) {
+          callback(chunk);
+        }
       }
     }
   }

src/neuroglancer/annotation/index.ts
@@ -491,7 +491,7 @@ export interface AnnotationSchema {
   properties: readonly AnnotationPropertySpec[];
 }
 
-function annotationToJson(annotation: Annotation, schema: AnnotationSchema) {
+export function annotationToJson(annotation: Annotation, schema: AnnotationSchema) {
   const result = getAnnotationTypeHandler(annotation.type).toJSON(annotation, schema.rank);
   result.type = AnnotationType[annotation.type].toLowerCase();
   result.id = annotation.id;

src/neuroglancer/datasource/brainmaps/backend.ts
@@ -772,14 +772,24 @@ function annotationToBrainmaps(annotation: Annotation): any {
       undefined :
       annotation.relatedSegments[0].map(x => x.toString());
   switch (annotation.type) {
-    case AnnotationType.LINE:
+    case AnnotationType.LINE: {
+      const {pointA, pointB} = annotation;
+      const size = vec3.subtract(vec3.create(), pointB as vec3, pointA as vec3);
+      return {
+        type: 'LINE',
+        corner: toCommaSeparated(pointA as vec3),
+        size: toCommaSeparated(size),
+        object_labels: objectLabels,
+        payload,
+      };
+    }
     case AnnotationType.AXIS_ALIGNED_BOUNDING_BOX: {
       const {pointA, pointB} = annotation;
       const minPoint = vector.min(vec3.create(), pointA, pointB);
       const maxPoint = vector.max(vec3.create(), pointA, pointB);
       const size = vec3.subtract(maxPoint, maxPoint, minPoint);
       return {
-        type: annotation.type === AnnotationType.LINE ? 'LINE' : 'VOLUME',
+        type: 'VOLUME',
         corner: toCommaSeparated(minPoint),
         size: toCommaSeparated(size),
         object_labels: objectLabels,

src/neuroglancer/datasource/brainmaps/frontend.ts
@@ -547,7 +547,7 @@ export class BrainmapsAnnotationSource extends MultiscaleAnnotationSourceBase {
     return [[{
       chunkSource: this.chunkManager.getChunkSource(BrainmapsAnnotationSpatialIndexSource, {
         parent: this,
-        spec: {limit: 1e9, ...spec},
+        spec: {limit: 0, chunkToMultiscaleTransform, ...spec},
         parameters: this.parameters,
         credentialsProvider: this.credentialsProvider,
       }),

src/neuroglancer/datasource/precomputed/frontend.ts
@@ -257,20 +257,15 @@ export class PrecomputedAnnotationSource extends MultiscaleAnnotationSourceBase 
   }
 
   getSources(): SliceViewSingleResolutionSource<AnnotationGeometryChunkSource>[][] {
-    const {rank} = this;
-    const {lowerBounds} = this.metadata.coordinateSpace.bounds;
     return [this.metadata.spatialIndices.map(spatialIndexLevel => {
-      const chunkToMultiscaleTransform = matrix.createIdentity(Float32Array, rank + 1);
-      for (let i = 0; i < rank; ++i) {
-        chunkToMultiscaleTransform[(rank + 1) * rank + i] = lowerBounds[i];
-      }
+      const {spec} = spatialIndexLevel;
       return {
         chunkSource: this.chunkManager.getChunkSource(PrecomputedAnnotationSpatialIndexSource, {
           parent: this,
-          spec: spatialIndexLevel.spec,
+          spec,
           parameters: spatialIndexLevel.parameters,
         }),
-        chunkToMultiscaleTransform,
+        chunkToMultiscaleTransform: spec.chunkToMultiscaleTransform,
       };
     })];
   }
@@ -609,8 +604,13 @@ class AnnotationMetadata {
           for (let i = 0; i < rank; ++i) {
             gridShapeInVoxels[i] = gridShape[i] * chunkShape[i];
           }
-          const spec = {
+          const chunkToMultiscaleTransform = matrix.createIdentity(Float32Array, rank + 1);
+          for (let i = 0; i < rank; ++i) {
+            chunkToMultiscaleTransform[(rank + 1) * rank + i] = lowerBounds[i];
+          }
+          const spec: AnnotationGeometryChunkSpecification = {
             limit,
+            chunkToMultiscaleTransform,
             ...makeSliceViewChunkSpecification({
               rank,
               chunkDataSize: chunkShape,

src/neuroglancer/rendered_data_panel.ts
@@ -591,6 +591,7 @@ export abstract class RenderedDataPanel extends RenderedPanel {
                   annotationLayer.source.update(annotationRef, newAnnotation);
                 },
                 (_event) => {
+                  annotationLayer.source.commit(annotationRef);
                   annotationRef.dispose();
                 });
           }

src/neuroglancer/sliceview/frontend.ts
@@ -531,6 +531,11 @@ gl_Position = uProjectionMatrix * aVertexPosition;
 export interface SliceViewSingleResolutionSource<Source extends SliceViewChunkSource =
                                                                     SliceViewChunkSource> {
   chunkSource: Source;
+
+  /**
+   * (rank + 1)*(rank + 1) homogeneous transformation matrix from the "chunk" coordinate space to
+   * the MultiscaleSliceViewChunkSource space.
+   */
   chunkToMultiscaleTransform: Float32Array;
 
   /**

src/neuroglancer/ui/annotations.css
@@ -165,6 +165,7 @@
 
 .neuroglancer-annotation-segment-item {
   color: black;
+  background-color: white;
 }
 
 .neuroglancer-annotations-view-dimension {

src/neuroglancer/ui/annotations.ts
@@ -20,7 +20,7 @@
 
 import './annotations.css';
 
-import {Annotation, AnnotationReference, AnnotationSource, AnnotationType, AxisAlignedBoundingBox, Ellipsoid, getAnnotationTypeHandler, Line} from 'neuroglancer/annotation';
+import {Annotation, AnnotationReference, AnnotationSource, AnnotationType, AxisAlignedBoundingBox, Ellipsoid, getAnnotationTypeHandler, Line, annotationToJson} from 'neuroglancer/annotation';
 import {AnnotationDisplayState, AnnotationLayerState} from 'neuroglancer/annotation/annotation_layer_state';
 import {MultiscaleAnnotationSource} from 'neuroglancer/annotation/frontend_source';
 import {AnnotationLayer, PerspectiveViewAnnotationLayer, SliceViewAnnotationLayer} from 'neuroglancer/annotation/renderlayer';
@@ -88,11 +88,15 @@ export class AnnotationSegmentListWidget extends RefCounted {
         if (annotation == null) {
           return;
         }
-        const {relatedSegments} = annotation;
-        const segments = relatedSegments === undefined ?
-            [...values] :
-            [...relatedSegments[this.relationshipIndex], ...values];
-        const newAnnotation = {...annotation, segments};
+        let {relatedSegments} = annotation;
+        if (relatedSegments === undefined) {
+          relatedSegments = this.annotationLayer.source.relationships.map(() => []);
+        } else {
+          relatedSegments = relatedSegments.slice();
+        }
+        const existingSegments = relatedSegments[this.relationshipIndex];
+        relatedSegments[this.relationshipIndex] = [...existingSegments, ...values];
+        const newAnnotation = {...annotation, relatedSegments};
         this.annotationLayer.source.update(this.reference, newAnnotation);
         this.annotationLayer.source.commit(this.reference);
       }));
@@ -162,10 +166,16 @@ export class AnnotationSegmentListWidget extends RefCounted {
         if (!event.ctrlKey) {
           return;
         }
-        const {relatedSegments} = annotation;
-        const existingSegments = (relatedSegments && relatedSegments[this.relationshipIndex]) || [];
+        let {relatedSegments} = annotation;
+        if (relatedSegments === undefined) {
+          relatedSegments = this.annotationLayer.source.relationships.map(() => []);
+        } else {
+          relatedSegments = relatedSegments.slice();
+        }
+        const existingSegments = relatedSegments[this.relationshipIndex];
         const newSegments = existingSegments.filter(x => !Uint64.equal(segment, x));
-        const newAnnotation = {...annotation, segments: newSegments ? newSegments : undefined};
+        relatedSegments[this.relationshipIndex] = newSegments;
+        const newAnnotation = {...annotation, relatedSegments};
         this.annotationLayer.source.update(this.reference, newAnnotation);
         this.annotationLayer.source.commit(this.reference);
       });
@@ -1355,7 +1365,11 @@ abstract class TwoStepAnnotationTool extends PlaceAnnotationTool {
         const state = this.inProgressAnnotation!;
         const reference = state.reference;
         const newAnnotation =
-            this.getUpdatedAnnotation(reference.value!, mouseState, annotationLayer);
+          this.getUpdatedAnnotation(reference.value!, mouseState, annotationLayer);
+        if (JSON.stringify(annotationToJson(newAnnotation, annotationLayer.source)) ===
+            JSON.stringify(annotationToJson(reference.value!, annotationLayer.source))) {
+          return;
+        }
         state.annotationLayer.source.update(reference, newAnnotation);
         this.layer.selectedAnnotation.value = {
           id: reference.id,

src/neuroglancer/annotation/frontend_source.spec.ts
@@ -1,0 +1,104 @@
+/**
+ * @license
+ * Copyright 2020 Google Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {Annotation, AnnotationId, AnnotationPropertySerializer, AnnotationPropertySpec, AnnotationSerializer, AnnotationType} from 'neuroglancer/annotation';
+import {deleteAnnotation, makeTemporaryChunk, updateAnnotation} from 'neuroglancer/annotation/frontend_source';
+
+class UpdateTester {
+  annotations: Annotation[] = [];
+  incrementalChunk = makeTemporaryChunk();
+  propertySerializer: AnnotationPropertySerializer;
+  constructor(rank: number, propertySpecs: readonly Readonly<AnnotationPropertySpec>[]) {
+    this.propertySerializer = new AnnotationPropertySerializer(rank, propertySpecs);
+  }
+  compare() {
+    const serializer = new AnnotationSerializer(this.propertySerializer);
+    for (const annotation of this.annotations) serializer.add(annotation);
+    const direct = serializer.serialize();
+    const incremental = this.incrementalChunk.data!.serializedAnnotations;
+    expect(direct.data).toEqual(incremental.data);
+    expect(direct.typeToIds).toEqual(incremental.typeToIds);
+    expect(direct.typeToOffset).toEqual(incremental.typeToOffset);
+    expect(direct.typeToIdMaps).toEqual(incremental.typeToIdMaps);
+  }
+  update(annotation: Annotation) {
+    const index = this.annotations.findIndex(x => x.id === annotation.id);
+    if (index === -1) {
+      this.annotations.push(annotation);
+    } else {
+      this.annotations[index] = annotation;
+    }
+    updateAnnotation(this.incrementalChunk.data!, annotation, this.propertySerializer);
+    this.compare();
+  }
+  delete(id: AnnotationId, type: AnnotationType) {
+    const index = this.annotations.findIndex(x => x.id === id);
+    expect(index).toBeGreaterThan(-1);
+    expect(this.annotations[index].type).toEqual(type);
+    this.annotations.splice(index, 1);
+    expect(deleteAnnotation(this.incrementalChunk.data!, type, id, this.propertySerializer)).toBe(true);
+    this.compare();
+  }
+}
+
+describe('updateAnnotations', () => {
+  it('rank 1', () => {
+    const tester = new UpdateTester(1, []);
+    tester.compare();
+
+    tester.update(
+        {id: 'a', type: AnnotationType.POINT, point: Float32Array.of(1, 2), properties: []});
+    tester.delete('a', AnnotationType.POINT);
+    tester.update(
+      {id: 'a', type: AnnotationType.POINT, point: Float32Array.of(1, 2), properties: []});
+
+    tester.update(
+        {id: 'b', type: AnnotationType.POINT, point: Float32Array.of(2, 3), properties: []});
+
+    tester.update({
+      id: 'c',
+      type: AnnotationType.LINE,
+      pointA: Float32Array.of(2, 3),
+      pointB: Float32Array.of(4, 5),
+      properties: []
+    });
+
+    tester.update({
+      id: 'd',
+      type: AnnotationType.LINE,
+      pointA: Float32Array.of(6, 7),
+      pointB: Float32Array.of(8, 9),
+      properties: []
+    });
+
+    tester.delete('a', AnnotationType.POINT);
+    tester.update({
+      id: 'e',
+      type: AnnotationType.ELLIPSOID,
+      center: Float32Array.of(6, 7),
+      radii: Float32Array.of(8, 9),
+      properties: []
+    });
+    tester.update({
+      id: 'f',
+      type: AnnotationType.AXIS_ALIGNED_BOUNDING_BOX,
+      pointA: Float32Array.of(6, 7),
+      pointB: Float32Array.of(8, 9),
+      properties: []
+    });
+    tester.delete('f', AnnotationType.AXIS_ALIGNED_BOUNDING_BOX);
+  });
+});