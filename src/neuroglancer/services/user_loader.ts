885658211f8fc5e461ec40a668ad48fd48e82526
Junjie Cheng <junjiecheng@cs.ucsd.edu>
2021-03-26 05:50:16 -0700

    Feature: Real-time state sharing ActiveBrainAtlasPipeline#11
    Users can work simultaneously in the multi-user mode by setting the GET
    parameter `multi` to 1. State will be updated in real-time (200ms per
    refresh) across different users.
    
    Co-authored-by: "Eddy O'Donnell" <eddyod@yahoo.com>

---
+++ src/neuroglancer/services/user_loader.ts
@@ -0,0 +1,61 @@
+import './user_loader.css';
+import {makeIcon} from 'neuroglancer/widget/icon';
+import {registerEventListener} from 'neuroglancer/util/disposable';
+import {userDataRef} from 'neuroglancer/services/firebase';
+import {urlParams, stateAPI, StateAPI} from 'neuroglancer/services/state_loader';
+
+const DATABASE_PORTAL = 'https://activebrainatlas.ucsd.edu/activebrainatlas/admin/neuroglancer/urlmodel/';
+
+export interface User {
+  user_id: number;
+  username: string;
+}
+
+export class UserLoader {
+  element = document.createElement('div');
+  private userList: HTMLElement;
+  users: any = [];
+  private stateAPI: StateAPI;
+  private user: User;
+
+  constructor() {
+    this.stateAPI = stateAPI;
+
+    if (urlParams.stateID) {
+      const stateID = urlParams.stateID;
+
+      this.stateAPI.getUser().then(jsonUser => {
+        this.user = jsonUser;
+      })
+
+      const usersRef = userDataRef.child(stateID).orderByKey();
+      usersRef.once("value").then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+          this.users.push(childSnapshot.val());
+        });
+        this.element.classList.add('user-loader');
+        this.userList = makeIcon({text: this.users.toString(), title: 'Logout'});
+        registerEventListener(this.userList, 'click', () => {
+          this.logout(stateID);
+        });
+        this.element.appendChild(this.userList);
+      });
+
+      userDataRef.on("child_changed", (snapshot) => {
+        if (snapshot.key === stateID) {
+          this.users = [];
+          snapshot.forEach((childSnapshot) => {
+            this.users.push(childSnapshot.val());
+          });
+          this.userList.textContent = this.users.toString();
+        }
+      });
+    }
+  }
+
+  private logout(state_id: string) {
+    userDataRef.child(state_id).child("" + this.user.user_id + "").remove();
+    window.location.href = DATABASE_PORTAL;
+  }
+}
+
