af7de0e466570b1f4c302abbfd38010309247c2f
Eddy O'Donnell <eddyod@yahoo.com>
2021-03-30 13:51:39 -0700

    Added a rest call to fetch animals in service.ts

--- src/neuroglancer/services/service.ts
+++ src/neuroglancer/services/service.ts
@@ -1,7 +1,33 @@
+import { fetchOk } from 'neuroglancer/util/http_request';
 
 
 export class AppSettings {
-  // public static API_ENDPOINT ='http://127.0.0.1:8000';
+  public static API_ENDPOINT ='http://127.0.0.1:8000';
   // public static DEV_API_ENDPOINT='http://127.0.0.1:8000';
-  public static API_ENDPOINT='https://activebrainatlas.ucsd.edu/activebrainatlas';
+  // public static API_ENDPOINT='https://activebrainatlas.ucsd.edu/activebrainatlas';
 }
+
+export class APIService {
+
+  constructor() { }
+
+
+  public async getAnimals(): Promise<any> {
+    const url = AppSettings.API_ENDPOINT + '/animals/'
+
+    try {
+      const response = await fetchOk(url, {
+        method: 'GET',
+      });
+      return await response.json();
+    } catch (err) {
+      console.log('Error in fetching animals ' + err);
+    }
+  }
+
+
+
+
+}
+
+
