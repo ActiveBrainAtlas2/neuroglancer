1af1c746ea3b3cf1b405969ce99e857dce71ea59
madwilliam5225 <madwilliam5225@gmail.com>
2021-08-13 17:39:19 -0700

    added drop down for structures

--- src/neuroglancer/services/service.ts
+++ src/neuroglancer/services/service.ts
@@ -1,3 +1,4 @@
 export class AppSettings {
-  public static API_ENDPOINT ='https://activebrainatlas.ucsd.edu/activebrainatlas';
+  // public static API_ENDPOINT ='https://activebrainatlas.ucsd.edu/activebrainatlas';
+  public static API_ENDPOINT ='http://localhost:8000';
 }
