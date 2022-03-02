d632efefc9774a53bbf4435ceb3e78cb6b1fc605
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-03-02 21:17:28 +0700

    Adddd more files to the merge with the main project

---
+++ src/neuroglancer/services/local.ts
@@ -0,0 +1,7 @@
+export const AppSettings = {
+        API_ENDPOINT: 'http://localhost:8000',
+        GOOGLE_LOGIN: 'https://webdev.dk.ucsd.edu/activebrainatlas/accounts/google/login/?next=',
+        LOCAL_LOGIN: 'https://webdev.dk.ucsd.edu/activebrainatlas/admin/login/?next=/devlogin',
+        DISPLAY_FETCH: true,
+        DISPLAY_GOOGLE: true
+};
