518accebfd6e13a86d349cd68789576e09f01051
bruce-wayne99 <makam.subramanyam.code@gmail.com>
2022-04-06 15:38:06 -0700

    Add polygon control sheet

--- src/neuroglancer/ui/polygon_options.ts
+++ src/neuroglancer/ui/polygon_options.ts
@@ -20,6 +20,92 @@
  import { Overlay } from 'neuroglancer/overlay';
  import { polygonRotateAngle, polygonSectionOffset, polygonScalePercentage } from '../annotation/polygon';
  import { StatusMessage } from '../status';
+ import './polygon_options.css';
+
+  interface PolygonControlSheetEntry {
+    function: string,
+    control: string,
+    mode: string,
+  };
+
+  export const polygonDocLink = "https://github.com/ActiveBrainAtlas2/neuroglancer/wiki/Neuroglancer-Contour-Annotations-Documentation";
+  export const polygonControlSheet : PolygonControlSheetEntry[] = [
+    {
+      function: "draw polygon vertex",
+      control: "left mouse click",
+      mode: "draw"
+    },
+    {
+      function: "close polygon",
+      control: "right mouse click",
+      mode: "draw"
+    },
+    {
+      function: "undo last vertex",
+      control: "key z",
+      mode: "draw"
+    },
+    {
+      function: "edit polygon vertex",
+      control: "left mouse click",
+      mode: "edit"
+    },
+    {
+      function: "add vertex to polygon",
+      control: "control+alt+left mouse click",
+      mode: "edit"
+    },
+    {
+      function: "delete vertex of polygon",
+      control: "control+alt+right mouse click",
+      mode: "edit"
+    },
+    {
+      function: "select polygon",
+      control: "control+right mouse click",
+      mode: "draw/edit/no op"
+    },
+    {
+      function: "translate polygon",
+      control: "shift+left mouse click",
+      mode: "edit"
+    },
+    {
+      function: "clone polygon",
+      control: "control+c (polygon needs to be selected first)",
+      mode: "draw/edit"
+    },
+    {
+      function: "rotate polygon",
+      control: "key r / key e (polygon needs to be selected first)",
+      mode: "edit"
+    },
+    {
+      function: "scale polygon",
+      control: "control+'+' / control+'-' (polygon needs to be selected first)",
+      mode: "edit"
+    },
+    {
+      function: "switch edit/noop -> draw mode",
+      control: "control+z",
+      mode: "edit/noop"
+    },
+    {
+      function: "switch draw/noop -> edit mode",
+      control: "control+x",
+      mode: "draw/noop"
+    },
+    {
+      function: "switch draw -> noop mode",
+      control: "control+z",
+      mode: "draw"
+    },
+    {
+      function: "switch edit -> noop mode",
+      control: "control+x",
+      mode: "edit"
+    }
+  ];
  
   export class PolygonOptionsDialog extends Overlay {
     sectionOffsetTextEle: HTMLInputElement
@@ -29,15 +115,16 @@
       super();
       
       const configTable = document.createElement('table');
+      configTable.classList.add('neuroglancer-polygon-options-config-table');
       configTable.caption = configTable.createCaption();
       configTable.caption.innerHTML = "<h2>Polygon configuration</h2>";
 
-      const coleGroupRow = this.getColGroupRow();
+      // const coleGroupRow = this.getColGroupRow();
       const cloneConfigRow = this.getCloneConfigRow();
       const scaleConfigRow = this.getScaleConfigRow();
       const rotateConfigRow = this.getRotateConfigRow();
 
-      configTable.appendChild(coleGroupRow);
+      // configTable.appendChild(coleGroupRow);
       configTable.appendChild(cloneConfigRow);
       configTable.appendChild(scaleConfigRow);
       configTable.appendChild(rotateConfigRow);
@@ -68,8 +155,14 @@
         this.dispose();
       });
 
-      this.content.appendChild(configTable);
-      this.content.appendChild(submitButton);
+      const cheatSheetTable = this.getCheatSheetTable();
+
+      const contentDiv = document.createElement('div');
+      contentDiv.classList.add('neuroglancer-polygon-options-content');
+      contentDiv.appendChild(configTable);
+      contentDiv.appendChild(submitButton);
+      contentDiv.appendChild(cheatSheetTable);
+      this.content.appendChild(contentDiv);
     }
 
     getColGroupRow() : HTMLTableColElement {
@@ -194,6 +287,41 @@
       element.appendChild(resetCell);
 
       return element;
+    }
+
+    getCheatSheetTable() : HTMLTableElement {
+      const table = document.createElement('table');
+      table.classList.add('neuroglancer-polygon-control-sheet');
+      table.caption = table.createCaption();
+      table.caption.innerHTML = `<h2>Polygon controls sheet <a href="${polygonDocLink}" target="_blank">[Wiki]</a></h2>`;
+
+      const headerRow = document.createElement('tr');
+      const keyCell = document.createElement('th');
+      keyCell.innerHTML = "function";
+      const controlCell = document.createElement('th');
+      controlCell.innerHTML = "key";
+      const modeCell = document.createElement('th');
+      modeCell.innerHTML = "mode";
+      headerRow.appendChild(keyCell);
+      headerRow.appendChild(controlCell);
+      headerRow.appendChild(modeCell);
+
+      table.appendChild(headerRow);
+
+      for(let row = 0; row < polygonControlSheet.length; row++) {
+        const rowEle = document.createElement('tr');
+        const functionCell = document.createElement('td');
+        functionCell.innerText = polygonControlSheet[row].function;
+        const controlCell = document.createElement('td');
+        controlCell.innerText = polygonControlSheet[row].control;
+        const modeCell = document.createElement('td');
+        modeCell.innerText = polygonControlSheet[row].mode;
+        rowEle.appendChild(functionCell);
+        rowEle.appendChild(controlCell);
+        rowEle.appendChild(modeCell);
+        table.appendChild(rowEle);
+      }
+      return table;
     }
   }
   
