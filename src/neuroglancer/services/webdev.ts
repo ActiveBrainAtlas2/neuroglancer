86686b5bdfed136136bec3bbefe7d97feeab2d8e
bruce-wayne99 <makam.subramanyam.code@gmail.com>
2022-05-03 01:38:14 -0700

    Revert webdev endpoint

--- src/neuroglancer/services/webdev.ts
+++ src/neuroglancer/services/webdev.ts
@@ -1,7 +1,7 @@
 export const AppSettings = {
-        API_ENDPOINT: 'https://webdev.dk.ucsd.edu/django_test',
-        GOOGLE_LOGIN: 'https://webdev.dk.ucsd.edu/django_test/accounts/google/login/?next=',
-        LOCAL_LOGIN: 'https://webdev.dk.ucsd.edu/django_test/admin/login/?next=',
+        API_ENDPOINT: 'https://webdev.dk.ucsd.edu/activebrainatlas',
+        GOOGLE_LOGIN: 'https://webdev.dk.ucsd.edu/activebrainatlas/accounts/google/login/?next=',
+        LOCAL_LOGIN: 'https://webdev.dk.ucsd.edu/activebrainatlas/admin/login/?next=',
         DISPLAY_FETCH: true,
         DISPLAY_GOOGLE: false
 };
