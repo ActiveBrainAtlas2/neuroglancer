6b7f272509cc33d7f89c798a6dfde7a9cd48f16e
Duane Rinehart <duane.rinehart@gmail.com>
2022-05-13 15:17:43 -0700

    added pons

---
+++ src/neuroglancer/services/pons.ts
@@ -0,0 +1,7 @@
+export const AppSettings = {
+        API_ENDPOINT: 'https://pons.dk.ucsd.edu:8000',
+        GOOGLE_LOGIN: 'https://pons.dk.ucsd.edu/activebrainatlas/accounts/google/login/?next=',
+        LOCAL_LOGIN: 'https://pons.dk.ucsd.edu/activebrainatlas/admin/login/?next=/devlogin',
+        DISPLAY_FETCH: true,
+        DISPLAY_GOOGLE: true
+};
