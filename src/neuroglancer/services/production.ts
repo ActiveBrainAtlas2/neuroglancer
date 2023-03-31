f9965f64b7cd40794dc65a0298aab199fd27207f
Edward O'Donnell <eddy.odonnell@gmail.com>
2023-03-31 11:45:37 +0700

    Updated the production API urls

--- src/neuroglancer/services/production.ts
+++ src/neuroglancer/services/production.ts
@@ -1,8 +1,8 @@
 export const AppSettings = {
-    API_ENDPOINT: 'https://brainsharer.org/brainsharer',
-    GOOGLE_LOGIN: 'https://brainsharer.org/brainsharer/accounts/google/login/?next=',
-    LOCAL_LOGIN: 'https://brainsharer.org/brainsharer/admin/login/?next=',
-    ADMIN_PORTAL: 'https://brainsharer.org/brainsharer/admin/',
+    API_ENDPOINT: 'https://www.brainsharer.org/brainsharer',
+    GOOGLE_LOGIN: 'https://www.brainsharer.org/brainsharer/accounts/google/login/?next=',
+    LOCAL_LOGIN: 'https://www.brainsharer.org/brainsharer/admin/login/?next=',
+    ADMIN_PORTAL: 'https://www.brainsharer.org/brainsharer/admin/',
     DISPLAY_FETCH: true,
     DISPLAY_GOOGLE: false
 };
